ae1ddb3984332984dd7cf8c33de95392fb1c025e
Hyfudiar <hyfudiarmusic@gmail.com>
2024-04-11 18:56:11 -0400

    Got access to web deployment, added it to the integration, and set up addressables

---
+++ LudumDare55/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,77 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AMBIENCE_TEST = 2925175128U;
+        static const AkUniqueID SWITCH_TEST = 2269466764U;
+        static const AkUniqueID TRIGGER_TEST1 = 2699324107U;
+        static const AkUniqueID TRIGGER_TEST2 = 2699324104U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace TESTSTATES
+        {
+            static const AkUniqueID GROUP = 763730847U;
+
+            namespace STATE
+            {
+                static const AkUniqueID LOWPASSED = 3975260481U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID NORMAL = 1160234136U;
+            } // namespace STATE
+        } // namespace TESTSTATES
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace TESTSWITCH
+        {
+            static const AkUniqueID GROUP = 2623550553U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID SWITCH1 = 1590382670U;
+                static const AkUniqueID SWITCH2 = 1590382669U;
+            } // namespace SWITCH
+        } // namespace TESTSWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID TESTPITCHRTPC = 4037818096U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID TESTSOUNDBANK = 1831431028U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
